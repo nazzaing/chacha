954a92663bface84760f466a734851d7b0a1ed93
nazzaing <41273919+nazzaing@users.noreply.github.com>
2023-06-08 22:54:28 +0900

    valide 예외처리 수정

--- src/main/java/com/chacha/post/controller/PostController.java
+++ src/main/java/com/chacha/post/controller/PostController.java
@@ -3,12 +3,10 @@ package com.chacha.post.controller;
 import com.chacha.post.dto.PostCreate;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @Slf4j
@@ -16,17 +14,7 @@ import java.util.Map;
 public class PostController {
 
     @PostMapping("/posts")
-    public Map<String, String> get(@RequestBody @Valid PostCreate params, BindingResult result) {
-        if (result.hasErrors()) {
-            List<FieldError> fieldErrors = result.getFieldErrors();
-            FieldError firstFieldError = fieldErrors.get(0);
-            String fieldName = firstFieldError.getField();
-            String errorMessage = firstFieldError.getDefaultMessage();
-
-            Map<String, String> error = new HashMap<>();
-            error.put(fieldName, errorMessage);
-            return error;
-        }
+    public Map<String, String> get(@RequestBody @Valid PostCreate params) {
         log.info("params={}", params);
         return Map.of();
     }
