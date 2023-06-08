292d186cda3ad1772ad3b7aae869ae17c3ae5203
nazzaing <41273919+nazzaing@users.noreply.github.com>
2023-06-08 20:34:51 +0900

    hello world 테스트 코드 작성

---
+++ src/test/java/com/chacha/post/controller/PostControllerTest.java
@@ -0,0 +1,27 @@
+package com.chacha.post.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest
+class PostControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("/posts 요청시 Hello World를 출력한다ㅑ.")
+    void test() throws Exception {
+        // expected
+        mockMvc.perform(MockMvcRequestBuilders.get("/posts"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("Hello World"))
+                .andDo(MockMvcResultHandlers.print());
+    }
+}
