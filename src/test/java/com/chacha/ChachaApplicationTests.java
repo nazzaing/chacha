3053fa638b034fc31f87ca5dc89a9a93cabfffa8
nazzaing <41273919+nazzaing@users.noreply.github.com>
2023-06-08 20:16:38 +0900

    first commit

---
+++ src/test/java/com/chacha/ChachaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chacha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChachaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
