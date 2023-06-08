5b1d92d5b34c09de7f7fc684dac131aae7d47ad7
nazzaing <41273919+nazzaing@users.noreply.github.com>
2023-06-08 21:57:37 +0900

    valid 유효성 검증 추가

--- src/main/java/com/chacha/post/controller/PostController.java
+++ src/main/java/com/chacha/post/controller/PostController.java
@@ -1,16 +1,33 @@
 package com.chacha.post.controller;
 
 import com.chacha.post.dto.PostCreate;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @RestController
 public class PostController {
 
     @PostMapping("/posts")
-    public String get(@RequestBody PostCreate params) {
+    public Map<String, String> get(@RequestBody @Valid PostCreate params, BindingResult result) {
+        if (result.hasErrors()) {
+            List<FieldError> fieldErrors = result.getFieldErrors();
+            FieldError firstFieldError = fieldErrors.get(0);
+            String fieldName = firstFieldError.getField();
+            String errorMessage = firstFieldError.getDefaultMessage();
+
+            Map<String, String> error = new HashMap<>();
+            error.put(fieldName, errorMessage);
+            return error;
+        }
         log.info("params={}", params);
-        return "Hello World";
+        return Map.of();
     }
 }
--- src/main/java/com/chacha/post/dto/PostCreate.java
+++ src/main/java/com/chacha/post/dto/PostCreate.java
@@ -1,20 +1,17 @@
 package com.chacha.post.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Setter
 @Getter
+@ToString
 public class PostCreate {
 
-    public String title;
-    public String content;
-
-    @Override
-    public String toString() {
-        return "PostCreate{" +
-                "title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
+    @NotBlank(message = "타이틀을 입력해주세요.")
+    private String title;
+    @NotBlank(message = "콘텐츠를 입력해주세요.")
+    private String content;
 }
